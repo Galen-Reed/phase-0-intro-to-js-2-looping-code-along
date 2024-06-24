14213709adf80d6e9d84f899d74c1450e6aacf8b
GalenReed <galenreed@vt.edu>
2024-06-24 11:20:23 -0400

    am i getting better at this?

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, name){
+    let newArray = [];
+    for (let i=0; i < array.length; i ++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    return newArray
+}
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+console.log(countDown(9));
